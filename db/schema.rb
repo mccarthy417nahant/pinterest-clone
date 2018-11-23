743889f8a40945a33a5e4eedd4d8a4c42e56ac2f
mccarthy417nahant <kevinmccarthy01@comcast.net>
2018-11-23 14:34:56 -0500

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_21_213420) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pins", force: :cascade do |t|
     t.string "title"
     t.text "description"
